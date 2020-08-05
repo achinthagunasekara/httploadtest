d7561a795d71099a22cc2d083bfbab49e3f043d2
Archie Gunasekara <agunasekara@slack-corp.com>
2020-08-05 01:41:10 -0700

    work so far

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/httploadtest
 
 go 1.12
 
-require github.com/sirupsen/logrus v1.6.0
+require (
+	github.com/dsoprea/go-logging v0.0.0-20200710184922-b02d349568dd // indirect
+	github.com/dsoprea/go-time-parse v0.0.0-20190110144354-e4bea0bc76a3
+	github.com/sirupsen/logrus v1.6.0
+)
--- main.go
+++ main.go
@@ -1,16 +1,81 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	log "github.com/sirupsen/logrus"
-	"http"
+	"net/http"
+	"time"
 )
 
-func main() {
-    log.Info("hello world")
+func getUrl(url string, timeout time.Duration, completed chan<- bool) {
+
 	httpClient := http.Client{Timeout: timeout}
-	resp, err := httpClient.Get("https://54dv9b2pi7.execute-api.us-east-1.amazonaws.com")
+	resp, err := httpClient.Get(url)
 	if err != nil {
-		log.Errorf("error with http call to: %v, details: %v", url, err)
+		log.Debugf("error with http call: %v", err)
+		completed <- false
+		return
 	}
-	log.Info(resp)
+	log.Debugf("http response status: %+v", resp.Status)
+	completed <- true
+}
+
+func main() {
+
+	url := flag.String("url", "", "url to run the load test against")
+	timeout := flag.Int("timeout", 30, "timeout for a given url")
+	concurrent := flag.Int("concurrent", 1, "number of tests to run concurrently")
+	duration := flag.String("duration", "30s", "duration to run tests for")
+	debug := flag.Bool("debug", false, "run in debug mode with more infomation on logs")
+	flag.Parse()
+
+	if *debug {
+		log.SetLevel(log.DebugLevel)
+		log.Debug("running in debug mode")
+	}
+
+	if *url == "" {
+		flag.PrintDefaults()
+		log.Fatal("flag `-url` must be passed in")
+	}
+
+	testStartedAt := time.Now()
+	phrasedDuration, err := time.ParseDuration(*duration)
+	if err != nil {
+		log.Fatal("please enter a valid duration for duration. e.g: 20s, 20m")
+	}
+	testEndAt := testStartedAt.Add(phrasedDuration)
+
+	successfulTests := 0
+	failedTests := 0
+	completed := make(chan bool)
+
+	log.Infof("load testing: %v", *url)
+
+	for i := 1; i <= *concurrent; i++ {
+		go getUrl(*url, time.Duration(*timeout)*time.Second, completed)
+	}
+
+	for success := range completed {
+
+		if success {
+			successfulTests += 1
+		} else {
+			failedTests += 1
+		}
+		log.Infof("test completed: %v", success)
+
+		timeNow := time.Now()
+		if testEndAt.Before(timeNow) {
+			break
+		}
+		go getUrl(*url, time.Duration(*timeout)*time.Second, completed)
+	}
+
+	fmt.Println("tested url: ", *url)
+	fmt.Println("test started at: ", testStartedAt)
+	fmt.Println("test ended at: ", testEndAt)
+	fmt.Println("successful tests: ", successfulTests)
+	fmt.Println("failed tests: ", failedTests)
 }
